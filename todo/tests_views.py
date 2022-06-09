e6ebf42cce485dcc5a64192e760e817555c4949c
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-06-09 22:35:11 +0000

    Add testing files

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -6,11 +6,4 @@ class TestDjango(TestCase):
     def test_this_thing_works(self):
         self.assertEqual(1, 1)
 
-    def test_this_thing_works2(self):
-        self.assertEqual(1, 1)
-
-    def test_this_thing_works3(self):
-        self.assertEqual(1,1)
-
-    def test_this_thing_works4(self):
-        self.assertEqual(1, 1)
+class Test
